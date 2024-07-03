da48449beb2e62d669a09ac7646702cc1d6c5a9c
EricNdivo <j.ericndivo@gmail.com>
2024-07-03 09:34:45 +0300

    last

--- kism_internal/settings.py
+++ kism_internal/settings.py
@@ -79,12 +79,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kism_internal',
         'USER': 'eric',
-        'PASSWORD': 'sentinel56',
+        'PASSWORD': 'sentinel90',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
