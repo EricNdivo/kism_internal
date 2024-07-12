b468f95fb4009c45cda2735c45bf87d71b77ad0b
EricNdivo <j.ericndivo@gmail.com>
2024-07-12 11:10:04 +0300

    kismlogo

--- kism_internal/settings.py
+++ kism_internal/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'kism_internal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '#',
-        'USER': '#',
-        'PASSWORD': '#',
+        'NAME': 'kism_internal',
+        'USER': 'eric',
+        'PASSWORD': 'sentinel90',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
