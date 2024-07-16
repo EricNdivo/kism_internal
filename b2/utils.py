56822cd74da19b3cd0d0ac4f816e0501b3a582d0
EricNdivo <j.ericndivo@gmail.com>
2024-07-16 10:16:59 +0300

    initial

--- b2/utils.py
+++ b2/utils.py
@@ -1,10 +1,24 @@
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
-def send_welcome_email(email):
-    subject = 'Logged in kism_admin'
-    message = render_to_string('certificates/welcome_email.html')
-    sender_email = 'j.ericndivo@gmail.com'
+def send_dispatch_email(email, dispatch_records):
+    subject = 'Dispatch Confirmation'
+    message = render_to_string('certificates/dispatch_email.html', {'dispatch_records': dispatch_records})
+    sender_email = settings.EMAIL_HOST_USER
     recipient_list = [email]
 
     send_mail(subject, message, sender_email, recipient_list)
+
+
+'''from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+def send_dispatch_confirmation_email(dispatch_records):
+    subject = 'Dispatch Confirmation'
+    email_body = render_to_string('certificates/dispatch_confirmation_email.html', {'dispatch_records': dispatch_records})
+    sender_email = settings.EMAIL_HOST_USER
+    recipient_list = [record.dispatched_to for record in dispatch_records]
+
+    send_mail(subject, email_body, sender_email, recipient_list, fail_silently=False)'''
--- kism_internal/settings.py
+++ kism_internal/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'j.ericndivo@gmail.com'
-EMAIL_HOST_PASSWORD = 'aepc nnwf wauu tghg'
+EMAIL_HOST_PASSWORD = 'jxfh oklk euzs gyej'
