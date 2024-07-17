41526663817d7593c1280e77f7c18306a85ce6ca
EricNdivo <j.ericndivo@gmail.com>
2024-07-17 11:32:50 +0300

    mailerrorhandling

--- b2/views.py
+++ b2/views.py
@@ -18,6 +18,36 @@ def certificate_list(request):
     return render(request, 'certificates/certificate_list.html', {'certificates': certificates})
 
 @login_required
+def add_certificate(request):
+    if request.method == 'POST':
+        form = CertificateRecordForm(request.POST, request.FILES)
+        certificate_number = request.POST.get('certificate_number')
+        uploaded_certificate = request.FILES.get('uploaded_certificate')
+
+        if CertificateRecord.objects.filter(certificate_number=certificate_number).exists():
+            messages.error(request, 'Certificate with this number already Exists')
+        elif not uploaded_certificate:
+            messages.error(request, 'Please upload a certificate.')
+        else:
+            if form.is_valid():
+                certificate = form.save(commit=False)
+                certificate.printed_by = request.user
+                certificate.printed = True
+                certificate.save()
+
+                today = timezone.now().date()
+                daily_record, created = DailyRecord.objects.get_or_create(date=today)
+                daily_record.printed_certificates.add(certificate)
+
+                messages.success(request, 'Certificate added Successfully.')
+                return redirect('certificate_list')
+            else:
+                messages.error(request, 'Form is not valid.')
+    else:
+        form = CertificateRecordForm()
+    return render(request, 'certificates/add_certificate.html', {'form': form})
+   
+@login_required
 def dispatch_certificate(request, certificate_id):
     certificate = get_object_or_404(CertificateRecord, id=certificate_id)
     
@@ -71,17 +101,23 @@ def dispatch_certificate(request, certificate_id):
                 
                 
                 dispatch_records = [dispatch_record]
-                if picked_by_email:
-                    send_dispatch_email(picked_by_email, dispatch_records)
-                
-                messages.success(request, 'Certificate dispatched successfully.')
-                return redirect('certificate_list')
+                try:
+                    if picked_by_email:
+                        send_dispatch_email(picked_by_email, dispatch_records)
+                    
+                    messages.success(request, 'Certificate dispatched successfully.')
+                    return redirect('certificate_list')
+
+                except Exception as e:
+                    messages.error(request, 'An error occurred while sending the dispatch email.')
+                    print(f"Error: {e}")
+                    return redirect('certificate_list')
+
             except IntegrityError:
                 messages.error(request, 'This certificate has already been dispatched.')
                 return redirect('certificate_list')
     else:
-        form = DispatchForm()
-    
+        form = DispatchForm() 
     return render(request, 'certificates/dispatch_certificate.html', {'certificate': certificate, 'form': form})
 
 @login_required
