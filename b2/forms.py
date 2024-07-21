8440f4b50c11492739a5f07d52be21dd862a7941
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-07-21 18:59:05 +0300

    revert email message

--- b2/forms.py
+++ b2/forms.py
@@ -12,6 +12,10 @@ class CertificateRecordForm(forms.ModelForm):
         }
         
 class DispatchForm(forms.ModelForm):
+    picked_by_email = forms.EmailField(required=False)
+    picked_by_phone = forms.CharField(max_length=15, required=False, label='Phone Number')
+    picked_by_wells_fargo = forms.BooleanField(required=False, label='Wells Fargo')
+
     class Meta:
         model = DispatchRecord
-        fields =['dispatched_to', 'dispatched_phone','picked_by_wells_fargo']
+        fields = ['picked_by_email', 'picked_by_phone', 'picked_by_wells_fargo']
